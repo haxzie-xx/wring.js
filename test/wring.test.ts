cb11a60f198566edd62c4855781d3f7b1b50e003
Musthaq Ahamad <musthu.gm@gmail.com>
2018-12-19 14:20:27 +0530

    Add sub collection formating

--- test/wring.test.ts
+++ test/wring.test.ts
@@ -5,12 +5,23 @@ import { Wring } from '../src';
 const wring = new Wring();
 const testYaml = 'test-strings.yml';
 const testResultYaml = 'test-result-strings.yml';
+const testObjectYaml = 'test-object-strings.yml';
+const testResultObjectYaml = 'test-result-object-strings.yml';
 
 // Type for the Yaml document
 type keys = string | number;
 type doc = {
     [K in keys] : any;
 }
+// test object with all values
+const testObject = {
+    user: 'haxzie',
+    number: 'first',
+    repo: 'wring',
+    1: 'haxzie',
+    2: 'probot',
+    3: 'wring'
+};
 
 test('Load the strings from yaml to a wring collection', () => {
     let collection = wring.load(testYaml);
@@ -28,14 +39,6 @@ test('Get all the items in a loaded collection', () => {
 
 
 test('Format all the test strings', () => {
-    let testObject = {
-        user: 'haxzie',
-        number: 'first',
-        repo: 'wring',
-        1: 'haxzie',
-        2: 'probot',
-        3: 'wring'
-    };
 
     let collection = wring.load(testYaml);
     let resultCollection = wring.load(testResultYaml);
@@ -45,4 +48,15 @@ test('Format all the test strings', () => {
         let result = collection.with(key).format(testObject);
         expect(result).toBe(resultCollection.get(key));
     }
+});
+
+test ('Check to use nested collections', () => {
+    let objectCollection = wring.load(testObjectYaml);
+    let collection = objectCollection.from('user');
+
+    let resultObjectCollection = yaml.safeLoad(fs.readFileSync(path.join(__dirname, testResultObjectYaml), 'utf-8'));
+
+    expect(collection.with('welcome').format(testObject)).toBe(resultObjectCollection['user']['welcome']);
+    expect(collection.with('greet').format(testObject)).toBe(resultObjectCollection['user']['greet']);
+
 });
